bc294d6a62c08afa49f4d3deda8f19bfc1a04d97
dev-081572 <47625392+dev-081572@users.noreply.github.com>
2019-04-07 17:51:26 +0300

    2.4 Добавлена навигация между страницами
    * Компонент Drawer - добавлена информация ссылкам

--- src/components/navigation/Drawer/Drawer.js
+++ src/components/navigation/Drawer/Drawer.js
@@ -1,15 +1,38 @@
 import React, {Component} from 'react';
 import styles from './Drawer.module.scss';
+import {NavLink} from 'react-router-dom';
 import Backdrop from '../../../UI/Backdrop/Backdrop';
 
-const links = [1, 2, 3];
+const links = [
+  {
+    label: 'Список',
+    to: '/',
+    exact: true
+  },
+  {
+    label: 'Авторизация',
+    to: '/auth',
+    exact: false
+  },
+  {
+    label: 'Создать тест',
+    to: '/quiz-creator',
+    exact: false
+  }
+];
 
 class Drawer extends Component {
   renderLinks() {
     return links.map((link, index) => {
       return (
         <li key = {index}>
-          <a>Link {link}</a>
+          <NavLink
+            to = {link.to}
+            exact = {link.exact}
+            activeClassName = {styles.active}
+          >
+            {link.label}
+          </NavLink>
         </li>
       );
     });
