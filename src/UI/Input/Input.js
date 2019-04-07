952e3fd3d7b373d1a71b03a42a7ea3b9f13d885a
dev-081572 <47625392+dev-081572@users.noreply.github.com>
2019-04-07 18:51:14 +0300

    3.3 Добавлена проверка на валидность компонента Input

--- src/UI/Input/Input.js
+++ src/UI/Input/Input.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import styles from './Input.module.scss';
 
+function isInvalid({valid, touched, shouldValidate}) {
+  return !valid && touched && shouldValidate;
+}
+
 const Input = props => {
   const inputType = props.type;
   const cls = [styles.Input];
   const htmlFor = `${inputType}-${Math.random()}`;
+
+  if (isInvalid(props)) {
+    cls.push(styles.invalid)
+  }
 
   return (
     <div className = {cls.join(' ')}>
@@ -17,7 +25,7 @@ const Input = props => {
         onChange = {props.onChange}
       />
 
-      <span>{props.errorMessage}</span>
+      {isInvalid(props) ? <span>{props.errorMessage}</span> : null}
     </div>
   )
 }
