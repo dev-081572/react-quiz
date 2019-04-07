4048b62564a971727616eec10e516ebd5cbf67da
dev-081572 <47625392+dev-081572@users.noreply.github.com>
2019-04-07 18:03:56 +0300

    2.6 Добавлен список доступных тестов

--- src/components/QuizList/QuizList.js
+++ src/components/QuizList/QuizList.js
@@ -1,11 +1,30 @@
 import React, {Component} from 'react';
 import styles from './QuizList.module.scss';
+import {Link} from 'react-router-dom';
 
 class QuizList extends Component {
+  renderQuizes() {
+    return [1, 2, 3].map((quiz, index) => {
+      return (
+        <li key = {index}>
+          <Link to = {`quiz/${quiz}`}>
+            {`Тест ${quiz}`}
+          </Link>
+        </li>
+      )
+    })
+  }
+
   render() {
     return (
       <div className = {styles.QuizList}>
-        <h1>QuizList</h1>
+        <div>
+          <h1>Список тестов</h1>
+
+          <ul>
+            {this.renderQuizes()}
+          </ul>
+        </div>
       </div>
     );
   }
