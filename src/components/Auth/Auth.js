d34b50ec6d3da59283289f5d3dac5d0f93aff4f6
dev-081572 <47625392+dev-081572@users.noreply.github.com>
2019-04-07 18:33:01 +0300

    3.1 Компонент Auth - добавлены верстка и стили

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,11 +1,34 @@
 import React, {Component} from 'react';
 import styles from './Auth.module.scss';
+import Button from '../../UI/Button/Button';
 
 class Auth extends Component {
+  loginHandler = () => {
+
+  }
+
+  registerHandler = () => {
+
+  }
+
+  submitHandler = event => {
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className = {styles.Auth}>
-        <h1>Auth</h1>
+        <div className = {styles.AuthWrapper}>
+          <h1>Авторизация</h1>
+
+          <form onSubmit = {this.submitHandler}>
+            <input type="text" />
+            <input type="text" />
+
+            <Button type="success" onClick = {this.loginHandler}>Войти</Button>
+            <Button type="primary" onClick = {this.registerHandler}>Зарегистрироваться</Button>
+          </form>
+        </div>
       </div>
     );
   }
