70502c4f2c02f68cec1d4fac3d167879b830bd2d
dev-081572 <47625392+dev-081572@users.noreply.github.com>
2019-04-07 23:18:46 +0300

    3.7 Компонент QuizCreator
    * Добавлены верстка и стили компоненту QuizCreator

--- src/components/QuizCreator/QuizCreator.js
+++ src/components/QuizCreator/QuizCreator.js
@@ -1,11 +1,55 @@
 import React, {Component} from 'react';
 import styles from './QuizCreator.module.scss';
+import Button from '../../UI/Button/Button';
 
 class QuizCreator extends Component {
+  addQuestionHandler = () => {
+
+  }
+
+  addQuizHandler = () => {
+
+  }
+
+  submitHandler = event => {
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className = {styles.QuizCreator}>
-        <h1>QuizCreator</h1>
+        <div className = {styles.QuizCreatorWrapper}>
+          <h1>Создать тест</h1>
+
+          <form onSubmit = {this.submitHandler}>
+            <input type="text" />
+            <hr />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+
+            <select>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+            </select>
+
+            <Button
+              type = 'success'
+              onClick = {this.addQuestionHandler}
+            >
+              Добавить вопрос
+            </Button>
+
+            <Button
+              type = 'primary'
+              onClick = {this.addQuizHandler}
+            >
+              Добавить тест
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
